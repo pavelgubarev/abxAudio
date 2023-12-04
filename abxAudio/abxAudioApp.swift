2c7faeac32e6a0cc0d3434bfc65830effcbc9ade
Губарев Павел <pavel.gubarev@gazprombank.ru>
2023-12-04 14:11:02 +0300

    Initial Commit

---
+++ abxAudio/abxAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  abxAudioApp.swift
+//  abxAudio
+//
+//  Created by GubarevPN on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct abxAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
